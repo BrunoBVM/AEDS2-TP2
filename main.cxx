01491aaeaa5e878fa834c016456f010c80a1eed7
BRMateus2 <mateusmatucumateixeira@gmail.com>
2017-05-30 21:41:22 -0300

    Update code, reduce memory requirements for benchmark;

--- main.cxx
+++ main.cxx
@@ -3,7 +3,7 @@
 #include "header.h"
 
 //Buffer
-#define BUF_DEF_AC_FIXED 256 //This is fixed because we wanna sort an full memory allocated, so we move structs from/in the array
+#define BUF_DEF_AC_FIXED 128 //This is fixed because we wanna sort an full memory allocated, so we move structs from/in the array
 
 
 void mateusmt_c_pause(){
@@ -21,6 +21,7 @@ struct s_intANDchar_key{
 };
 
 int main(){
+	printf("s_int_key size: %d bytes\ts_intANDchar_key size: %d bytes\n", sizeof(struct s_int_key), sizeof(struct s_intANDchar_key));
 	mateusmt_c_pause();
 	return 0;
 }
