57f270cb2de8aaf2594320d2571054d5bf5238ba
BrunoBVM <bruno.bvm@gmail.com>
2017-06-04 10:21:06 -0300

    Update a_insertionsort.cxx

--- a_insertionsort.cxx
+++ a_insertionsort.cxx
@@ -1,6 +1,22 @@
 //a_insertionsort.cxx
 #include "header.h"
 
+void insertionSort(int array[], int arraysize)
+{
+    int i, aux, j;
+    for (i = 0; i <= arraysize; i++)
+    {
+        aux = array[i];
+        j = i-1;
+        
+        while (j >= 0 and array[j] > aux)
+        {
+            array[j+1] = array[j];
+            j = j-1;
+        }
+        array[j+1] = aux;
+    }
+}
 
 /*Bibliografia:
 
