3c50ecfd1331cff6cf9b890ad5bb9136673ea0f2
BRMateus2 <mateusmatucumateixeira@gmail.com>
2017-06-05 00:14:39 -0300

    Correções sem teste, conforme Wikipedia
    Precisa preencher bibliografia

--- a_insertionsort.cxx
+++ a_insertionsort.cxx
@@ -4,17 +4,17 @@
 void insertionSort(int array[], int arraysize)
 {
     int i, aux, j;
-    for (i = 0; i <= arraysize; i++)
+    for (i = 0; i < arraysize; i++)
     {
         aux = array[i];
-        j = i-1;
+        j = (i - 1);
         
-        while (j >= 0 and array[j] > aux)
+        while ((j >= 0) and (array[j] > aux))
         {
-            array[j+1] = array[j];
-            j = j-1;
+            array[(j + 1)] = array[j];
+            j = (j - 1);
         }
-        array[j+1] = aux;
+        array[(j + 1)] = aux;
     }
 }
 
